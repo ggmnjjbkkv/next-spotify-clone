d60f9defc49f98fd22c3c5a1743804cfeb7ca418
Gerald Gonzales <94728635+ggmnjjbkkv@users.noreply.github.com>
2024-01-09 09:54:22 +0800

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,10 @@
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { twMerge } from 'tailwind-merge';
-import { RxCaretLeft } from "react-icons/rx"
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import Button from './Button';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -43,14 +46,98 @@ const Header: React.FC<HeaderProps> = ({
         gap-x-2
         items-center
         '>
-          <button>
+          <button 
+          onClick={() => router.back()} 
+          className='
+          rounded-full
+          bg-black
+          flex
+          items-center
+          justify-center
+          hover:opacity-75
+          transition
+          '>
             <RxCaretLeft 
             size={35}
             className='text-white'
             />
           </button>
+          <button
+          onClick={() => router.forward()} 
+          className='
+          rounded-full
+          bg-black
+          flex
+          items-center
+          justify-center
+          hover:opacity-75
+          transition
+          '>
+            <RxCaretRight 
+            size={35}
+            className='text-white'
+            />
+          </button>
+        </div>
+        <div className='flex md:hidden gap-x-2 items-center'>
+          <button className='
+          rounded-full
+          p-2
+          bg-white
+          flex
+          items-center
+          justify-content
+          hover:opacity-75
+          transition
+          '>
+            <HiHome className='text-black' size={20} />
+          </button>
+          <button className='
+          rounded-full
+          p-2
+          bg-white
+          flex
+          items-center
+          justify-content
+          hover:opacity-75
+          transition
+          '>
+            <BiSearch className='text-black' size={20} />
+          </button>
+        </div>
+        <div className='
+        flex
+        justify-between
+        items-center
+        gap-x-4
+        '>
+          <>
+          <div>
+            <Button
+            onClick={() => {}} 
+            className='
+            bg-transparent
+            text-neutral
+            font-medium
+            '>
+              Sign up
+            </Button>
+          </div>
+          <div>
+            <Button
+            onClick={() => {}}  
+            className='
+            bg-white
+            px-6
+            py-2
+            '>
+              Log in
+            </Button>
+          </div>
+          </>
         </div>
       </div>
+      {children}
     </div>
   )
 }
