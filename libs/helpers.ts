5142e863edcfec71d6d2e70e570516dc688c4fab
Gerald Gonzales <94728635+ggmnjjbkkv@users.noreply.github.com>
2024-01-21 11:37:37 +0800

    commit

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -4,7 +4,7 @@ export const getUrl = () => {
   let url =
   process.env.NEXT_PUBLIC_SITE_URL ??
   process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-  'http://localhost:3000/';
+  'https://next-spotify-clone-five.vercel.app/';
 
   url = url.includes('http') ? url : `https://${url}`;
 
